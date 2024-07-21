794f51a28222e136a708d23e02a53478e2d3d504
the-usman <theusman620@gmail.com>
2024-07-21 22:37:15 +0500

    Final Changing

--- src/components/BWT.jsx
+++ src/components/BWT.jsx
@@ -7,7 +7,7 @@ const BWT = () => {
             <div className='flex gap-5'>
                 <div className="box bg-[#E3D1E8] p-5 w-[35vw] rounded-xl h-[90px] flex items-center justify-between">
                     <div>
-                        <div className="heading font-[700] text-[16px]">Use AI to generate text</div><div className="heading font-[400] text-[16px] text-[#8F4F96]">Use AI to generate text</div>
+                        <div className="heading font-[700] text-[16px]">Use AI to generate text</div><div className="heading font-[400] text-[16px] text-[#8F4F96]">Try it out in the ChatGPT plugin</div>
                     </div>
                     <button className="bg-[#9612A3] h-[40px] text-white rounded-xl w-[113.03px]">
                         Open Plugin
@@ -15,7 +15,7 @@ const BWT = () => {
                 </div>
                 <div className="box bg-[#E3D1E8] p-5 w-[35vw] rounded-xl h-[90px] flex items-center justify-between">
                     <div>
-                        <div className="heading font-[700] text-[16px]">Use AI to generate text</div><div className="heading font-[400] text-[16px] text-[#8F4F96]">Use AI to generate text</div>
+                        <div className="heading font-[700] text-[16px]">Use AI to generate text</div><div className="heading font-[400] text-[16px] text-[#8F4F96]">Try it out in the ChatGPT plugin</div>
                     </div>
                     <button className="bg-[#9612A3] h-[40px] text-white rounded-xl w-[113.03px]">
                         Open Plugin
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -34,21 +34,21 @@ const Sidebar = () => {
                     Create new site
                 </button>
                 <div className={sidebarcss.innerSection}>
-                    <div className={`active mt-2 ${sidebarcss.elementInUpper}`}>
-                        <Image src="/pause.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Pause icon" />
-                        Dashboard
+                    <div className={`mt-2 ${sidebarcss.elementInUpper}`}>
+                        <Image src="/docs.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Pause icon" />
+                        Docs
                     </div>
                     <div className={sidebarcss.elementInUpper}>
-                        <Image src="/side.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Site icon" />
-                        Site
+                        <Image src="/toturial.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Site icon" />
+                        Tutorials
                     </div>
                     <div className={sidebarcss.elementInUpper}>
-                        <Image src="/teams.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Teams icon" />
-                        Teams
+                        <Image src="/help.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Teams icon" />
+                        Help & feedback
                     </div>
                     <div className={sidebarcss.elementInUpper}>
-                        <Image src="/upgrade.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Upgrade icon" />
-                        Upgrades
+                        <Image src="/api.svg" width={24} height={24} style={{ paddingRight: 0 }} alt="Upgrade icon" />
+                        API reference
                     </div>
                 </div>
 
